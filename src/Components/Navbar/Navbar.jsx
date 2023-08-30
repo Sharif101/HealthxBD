2d5eeb97f650ddeb810e57739ab24ae7a7da056e
Sharif101 <sharif009009.com>
2023-08-30 12:27:20 +0600

    navbar responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,23 @@
-
-import React from 'react';
-import classes from './Navbar.module.css'
-import logo from '../../assets/Navbar/Logo.png'
+import React from "react";
+import classes from "./Navbar.module.css";
+import logo from "../../assets/Navbar/Logo.png";
 export default function Navbar() {
   return (
     <div className={classes.container}>
-     <div className={classes.logo}>
-         <img src={logo} alt="" />
-         <p>Logoipsum</p>
-     </div>
-     <div className={classes.nav_item}>
-         <a href="">About Us</a>
-         <a href="">Services</a>
-         <a href="">Team</a>
-         <a href="">Contact</a>
-         <a href="">Blog</a>
-         <button>login</button>
-     </div>
- </div>
-  )
+      <div className={classes.logo}>
+        <img src={logo} alt="" />
+        <p>Logoipsum</p>
+      </div>
+      <div className={classes.navItem}>
+        <div className={classes.navLink}>
+          <a href="">About Us</a>
+          <a href="">Services</a>
+          <a href="">Team</a>
+          <a href="">Contact</a>
+          <a href="">Blog</a>
+        </div>
+        <button>login</button>
+      </div>
+    </div>
+  );
 }
